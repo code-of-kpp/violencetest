5fe20840d5ed81d99ceb6e7b581c9777045f7a61
Konstantin Podshumok <kpp.live@gmail.com>
2014-02-23 16:31:16 +0400

    fix urls

--- stopviolence/urls.py
+++ stopviolence/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('stopviolence.views',
-    url('^(?P<theme>[a-zA-Z])/ask', 'index',),
-    url('^(?P<theme>[a-zA-Z])', 'result',),
+    url('^(?P<theme>[a-zA-Z]+)/ask', 'index',),
+    url('^(?P<theme>[a-zA-Z]+)', 'result',),
 )
--- stopviolence/views.py
+++ stopviolence/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
+
+from .models import ViolentPhoto, Theme
 
 
 def result(request, theme):
-    return render_to_response('result.html')
+    theme = get_object_or_404(Theme, short_name=theme)
+
+    vp = ViolentPhoto.objects.filter(theme=theme).all()
+
+    return render_to_response('result.html', {'photos': vp})
 
 
 def index(request, theme):
